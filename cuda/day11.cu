64290e74114b9ce7268bb6dfe71d3758ef7f5aab
bobowedge <mmbowen@gmail.com>
2021-03-24 14:20:36 -0400

    Updates for Day 11

--- cuda/day11.cu
+++ cuda/day11.cu
@@ -3,47 +3,84 @@
 #include <inttypes.h>
 #include <string>
 
-const int64_t PWDLEN = 8;
+// Arbitrary choice of blocks and threads
 const int64_t BLOCKS = 64;
 const int64_t THREADS = 64;
 
-__device__ __managed__ int64_t nextN = 0;
+// Fixed password length
+const int64_t PWDLEN = 8;
 
+// Solution integer
+__device__ __managed__ int64_t soln = 0;
+
+// Check if a given password meets the rules
 __device__ bool is_valid(const char pwd[PWDLEN])
 {
-    bool rule2 = false;
+    bool rule1 = false;
     bool rule3 = false;
-    int64_t overlap = 0;
+    int64_t rule3Value = 0;
     for (int64_t i = 0; i < PWDLEN; ++i)
     {
+        // Rule 2
         if (pwd[i] == 'i' || pwd[i] == 'o' || pwd[i] == 'l')
         {
             return false;
         }
         if (i < PWDLEN - 1)
         {
+            // Rule 3
             if (pwd[i] == pwd[i+1])
             {
-                if (overlap != 0 && pwd[i] != overlap)
+                if (rule3Value != 0 && pwd[i] != rule3Value)
                 {
                     rule3 = true;
                 }
-                overlap = pwd[i];
+                rule3Value = pwd[i];
             }
+            // Rule 1
             else if (i < PWDLEN - 2)
             {
                 if ( (pwd[i] + 1) == pwd[i + 1] &&
                      (pwd[i+1] + 1) == pwd[i + 2])
                 {
-                    rule2 = true;
+                    rule1 = true;
                 }
             }
         }
     }
-    return rule2 & rule3;
+    return rule1 & rule3;
 }
 
-__device__ __host__ int64_t pwd_to_int(const char pwd[PWDLEN])
+// Convert an integer to a password on the device
+__device__ void int_to_pwd(int64_t x, char pwd[PWDLEN])
+{
+    for (int64_t i = PWDLEN - 1; i >= 0; --i)
+    {
+        pwd[i] = (x % 26) + 'a';
+        x /= 26;
+    }
+}
+
+// Find the next valid password
+__global__ void next_valid_pwd(int64_t intPwd)
+{
+    // Buffer for storing password string
+    char pwd[PWDLEN];
+    for (int64_t N = intPwd + threadIdx.x + blockIdx.x * blockDim.x; N < soln; N +=  blockDim.x * gridDim.x)
+    {
+        // Convert password integer to string
+        int_to_pwd(N, pwd);
+        // Check validity of password
+        if (is_valid(pwd))
+        {
+            atomicMin(&soln, N);
+            break;
+        }
+    }
+}
+
+// Convert a password to an integer on the host
+__host__ int64_t pwd_to_int(const std::string pwd)
 {
     int64_t N = 0;
     int64_t power = 1;
@@ -55,20 +92,7 @@ __device__ __host__ int64_t pwd_to_int(const char pwd[PWDLEN])
     return N;
 }
 
-__host__ int64_t pwd_to_int(const std::string pwd)
-{
-    return pwd_to_int(pwd.c_str());
-}
-
-__device__ void int_to_pwd(int64_t x, char pwd[PWDLEN])
-{
-    for (int64_t i = PWDLEN - 1; i >= 0; --i)
-    {
-        pwd[i] = (x % 26) + 'a';
-        x /= 26;
-    }
-}
-
+// Convert an integer to a password on the host
 __host__ std::string int_to_pwd(int64_t x)
 {
     std::string pwd('a', 8);
@@ -80,49 +104,28 @@ __host__ std::string int_to_pwd(int64_t x)
     return pwd;
 }
 
-__global__ void next_pwd(char* pwd)
-{
-    int64_t start = threadIdx.x + blockIdx.x * blockDim.x;
-    int64_t stop = static_cast<int64_t>(__powf(26, PWDLEN));
-    int64_t increment = blockDim.x * gridDim.x;
-    int64_t Npwd = pwd_to_int(pwd);
-    char newPwd[PWDLEN];
-    for (int64_t x = start; x < stop; x += increment)
-    {
-        int64_t newN = Npwd + x;
-        int_to_pwd(newN, newPwd);
-        if (is_valid(newPwd))
-        {
-            atomicMin(&nextN, newN);
-            break;
-        }
-    }
-}
-
 int main()
 {
+    // Input value
     std::string pwd = "hepxcrrq";
-    nextN = 208827064576;
+    // Convert to integer
+    int64_t intPwd = pwd_to_int(pwd);
 
-    char* dev_pwd;
-    size_t pwdSize = PWDLEN * sizeof(char);
-    cudaMalloc((void**)&dev_pwd, pwdSize);
-    cudaMemcpy(dev_pwd, pwd.data(), pwdSize, cudaMemcpyHostToDevice);
+    // Max possible solution (corresponds to "zzzzzzzz")
+    soln = static_cast<int64_t>(powf(26, PWDLEN));
 
-    next_pwd<<<BLOCKS,THREADS>>>(dev_pwd);
+    next_valid_pwd<<<BLOCKS,THREADS>>>(intPwd);
     cudaDeviceSynchronize();
 
-    std::cout << "Solution 1: " << int_to_pwd(nextN) << std::endl;
+    std::cout << "Solution 1: " << int_to_pwd(soln) << std::endl;
 
-    ++nextN;
-    pwd = int_to_pwd(nextN);
-    nextN = 208827064576;
-    cudaMemcpy(dev_pwd, pwd.data(), pwdSize, cudaMemcpyHostToDevice);
+    // Increment above previous password
+    intPwd = soln + 1;
+    // Max possible new solution
+    soln = static_cast<int64_t>(powf(26, PWDLEN));
 
-    next_pwd<<<BLOCKS,THREADS>>>(dev_pwd);
+    next_valid_pwd<<<BLOCKS,THREADS>>>(intPwd);
     cudaDeviceSynchronize();
 
-    std::cout << "Solution 2: " << int_to_pwd(nextN) << std::endl;
-
-    cudaFree(dev_pwd);
+    std::cout << "Solution 2: " << int_to_pwd(soln) << std::endl;
 }
