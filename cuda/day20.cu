8506c20e391a95ff91865acf4a1dd9141c6ca293
bobowedge <mmbowen@gmail.com>
2021-04-07 12:38:20 -0400

    Updates for Day 20

--- cuda/day20.cu
+++ cuda/day20.cu
@@ -1,21 +1,25 @@
+#include <cassert>
+#include <chrono>
 #include <cstdint>
 #include <iostream>
-#include <chrono>
 #include <vector>
 
-const uint64_t PUZZLE_INPUT = 36000000;
-const uint64_t MAX_HOUSES1 = PUZZLE_INPUT/10;
-const uint64_t MAX_HOUSES2 = PUZZLE_INPUT/11 + 1;
+// Given number of input presents
+const uint32_t PUZZLE_INPUT = 36000000;
+// Maximum house number for Part 1
+const uint32_t MAX_HOUSES1 = PUZZLE_INPUT/10;
+// Maximum house number for Part 2
+const uint32_t MAX_HOUSES2 = PUZZLE_INPUT/11 + 1;
 
-const uint64_t BLOCKS = 1024;
-const uint64_t THREADS = 256;
+// House stack for Part 1
+__device__ __managed__ uint32_t* houses1;
+// House stack for Part 2
+__device__ __managed__ uint32_t* houses2;
 
-__device__ __managed__ uint64_t* houses1;
-__device__ __managed__ uint64_t* houses2;
-
+// Reset the house stacks to 0
 __global__ void reset_houses()
 {
-    for(uint64_t house = threadIdx.x + blockIdx.x * blockDim.x;
+    for(uint32_t house = threadIdx.x + blockIdx.x * blockDim.x;
         house < MAX_HOUSES1;
         house += gridDim.x * blockDim.x)
     {
@@ -27,121 +31,154 @@ __global__ void reset_houses()
     }
 }
 
+// Set the present for house (Part 1)
+__device__ void set_house1(uint32_t house, uint32_t presents)
+{
+    if (houses1[house] + presents >= PUZZLE_INPUT)
+    {
+        atomicExch(&houses1[house], PUZZLE_INPUT);
+    }
+    else
+    {
+        atomicAdd(&houses1[house], presents);
+    }
+}
+
+// Set the present for house (Part 2)
+__device__ void set_house2(uint32_t house, uint32_t presents)
+{
+    if (houses2[house] + presents >= PUZZLE_INPUT)
+    {
+        atomicExch(&houses2[house], PUZZLE_INPUT);
+    }
+    else
+    {
+        atomicAdd(&houses2[house], presents);
+    }
+}
+
+// Deliver the present house by house
 __global__ void deliver_by_house()
 {
-    for(uint64_t house = threadIdx.x + blockIdx.x * blockDim.x;
+    // Loop over the houses (starting at 1)
+    for(uint32_t house = 1 + threadIdx.x + blockIdx.x * blockDim.x;
         house < MAX_HOUSES1;
         house += gridDim.x * blockDim.x)
     {
-        uint64_t presents1 = 0;
-        uint64_t presents2 = 0;
-        for (uint64_t j = 1; j <= house; ++j)
+        // Loop over the possible elfs for this house
+        for (uint32_t elf = 1; elf <= house; ++elf)
         {
-            if ((house % j) == 0)
+            // Does this elf deliver here (i.e. is elf a factor of house)?
+            if ((house % elf) == 0)
             {
-                presents1 += 10 * j;
-                if (j >= house/50 && j < MAX_HOUSES2)
+                // Set presents for houses1
+                set_house1(house, 10 * elf);
+                // Does this elf deliver here (Part 2)?
+                if (house < MAX_HOUSES2 && elf >= house/50)
                 {
-                    presents2 += 11 * j;
+                    // Set presents for houses2
+                    set_house2(house, 11 * elf);
                 }
             }
         }
-        houses1[house] = presents1;
-        if (house < MAX_HOUSES2)
-        {
-            houses2[house] = presents2;
-        }
     }
 }
 
+// Deliver the present elf by elf
 __global__ void deliver_by_elf()
 {
-    for(uint64_t elf = threadIdx.x + blockIdx.x * blockDim.x;
+    // Loop over elves (starting at 1)
+    for(uint32_t elf = 1 + threadIdx.x + blockIdx.x * blockDim.x;
         elf < MAX_HOUSES1;
         elf += gridDim.x * blockDim.x)
     {
-        if (elf == 0)
-            continue;
-        uint64_t presents1 = 10 * elf;
-        for (uint64_t j = elf; j < MAX_HOUSES1; j += elf)
+        // Presents for this elf (Part 1)
+        for (uint32_t j = elf; j < MAX_HOUSES1; j += elf)
         {
-            if (houses1[j] + presents1 >= PUZZLE_INPUT)
-                atomicExch(&houses1[j], PUZZLE_INPUT);
-            else
-                atomicAdd(&houses1[j], presents1);
+            set_house1(j, 10 * elf);
         }
-        uint64_t presents2 = 11 * elf;
-        for (uint64_t j = elf; j <= 50 * elf && j < MAX_HOUSES2; j += elf)
+        // Presents for this elf (Part 2)
+        for (uint32_t j = elf; j <= 50 * elf && j < MAX_HOUSES2; j += elf)
         {
-            if (houses2[j] + presents2 >= PUZZLE_INPUT)
-                atomicExch(&houses2[j], PUZZLE_INPUT);
-            else
-                atomicAdd(&houses2[j], presents2);
+            set_house2(j, 11 * elf);
         }
     }
 }
 
-uint64_t check_solution1()
+// After the house are delivereed, find the smallest solution
+__host__ uint32_t check_solution1()
 {
-    for (uint64_t i = 1; i < MAX_HOUSES1; ++i)
+    for (uint32_t house = 1; house < MAX_HOUSES1; ++house)
     {
-        if (houses1[i] >= PUZZLE_INPUT)
+        if (houses1[house] >= PUZZLE_INPUT)
         {
-            return i;
+            return house;
         }
     }
     return ULONG_MAX;
 }
 
-uint64_t check_solution2()
+// After the house are delivereed, find the smallest solution
+__host__ uint32_t check_solution2()
 {
-    for (uint64_t i = 1; i < MAX_HOUSES2; ++i)
+    for (uint32_t house = 1; house < MAX_HOUSES2; ++house)
     {
-        if (houses2[i] >= PUZZLE_INPUT)
+        if (houses2[house] >= PUZZLE_INPUT)
         {
-            return i;
+            return house;
         }
     }
     return ULONG_MAX;
+}
+
+void timing_code(uint32_t blocks, uint32_t threads, bool by_elf)
+{
+    // Initialize the houses to 0
+    reset_houses<<<blocks,threads>>>();
+    cudaDeviceSynchronize();
+
+    auto start = std::chrono::high_resolution_clock::now();
+    if (by_elf)
+    {
+        deliver_by_elf<<<blocks,threads>>>();
+        std::cout << "By elf: ";
+    }
+    else
+    {
+        deliver_by_house<<<blocks,threads>>>();
+        std::cout << "By house: ";
+    }
+    cudaDeviceSynchronize();
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout << blocks << " blocks, " << threads << " threads, ";
+    std::cout << std::chrono::duration<double>(end - start).count() << " seconds\n";
+
+    auto soln1 = check_solution1();
+    assert(soln1 == 831600);
+    auto soln2 = check_solution2();
+    assert(soln2 == 884520);
+    return;
 }
 
 int main()
 {
-    cudaMallocManaged(&houses1, MAX_HOUSES1 * sizeof(uint64_t));
-    cudaMallocManaged(&houses2, MAX_HOUSES2 * sizeof(uint64_t));
+    // Set up housing heaps
+    cudaMallocManaged(&houses1, MAX_HOUSES1 * sizeof(uint32_t));
+    cudaMallocManaged(&houses2, MAX_HOUSES2 * sizeof(uint32_t));
 
-    reset_houses<<<BLOCKS,THREADS>>>();
-    cudaDeviceSynchronize();
-
-    // std::vector<uint64_t> blocks = {32, 64, 128, 256, 512, 1024};
-    // std::vector<uint64_t> threads = {128, 256};
-
-    auto start = std::chrono::high_resolution_clock::now();
-    deliver_by_house<<<BLOCKS,THREADS>>>();
-    cudaDeviceSynchronize();
-    auto end = std::chrono::high_resolution_clock::now();
-    std::cout << BLOCKS << " blocks, " << THREADS << " threads, ";
-    std::cout << std::chrono::duration<double>(end - start).count() << " seconds\n";
-
-    uint64_t soln1 = check_solution1();
-    uint64_t soln2 = check_solution2();
-    std::cout << "Solution 1: " << soln1 << std::endl;
-    std::cout << "Solution 2: " << soln2 << std::endl;
-
-    reset_houses<<<BLOCKS,THREADS>>>();
-    cudaDeviceSynchronize();
-
-    start = std::chrono::high_resolution_clock::now();
-    deliver_by_elf<<<BLOCKS,THREADS>>>();
-    cudaDeviceSynchronize();
-    end = std::chrono::high_resolution_clock::now();
-    std::cout << BLOCKS << " blocks, " << THREADS << " threads, ";
-    std::cout << std::chrono::duration<double>(end - start).count() << " seconds\n";
-
-    soln1 = check_solution1();
-    soln2 = check_solution2();
-    std::cout << "Solution 1: " << soln1 << std::endl;
-    std::cout << "Solution 2: " << soln2 << std::endl;
+    // Timing for different numbers of blocks and threads
+    std::vector<uint32_t> blocksValues = {32, 64, 128, 256, 512, 1024};
+    std::vector<uint32_t> threadsValues = {128, 256};
+    for (auto by_elf : {true, false})
+    {
+        for (auto blocks : blocksValues)
+        {
+            for(auto threads : threadsValues)
+            {
+                timing_code(blocks, threads, by_elf);                
+            }
+        }
+    }
 
     cudaFree(houses1);
     cudaFree(houses2);
