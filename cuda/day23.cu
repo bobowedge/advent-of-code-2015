06f7a8b53f53b72fa99460a4f8e77282e03587c2
bobowedge <mmbowen@gmail.com>
2021-04-07 15:18:11 -0400

    Updates for Day 23

--- cuda/day23.cu
+++ cuda/day23.cu
@@ -1,43 +1,53 @@
 #include <iostream>
 
+// Starting value for register a
 __device__ __managed__ int64_t a;
+// Starting value for register b
 __device__ __managed__ int64_t b;
 
+// Function pointer alias
 typedef void (*fptr)(int64_t&, int64_t&);
 
+// Cut value in half
 __device__ void hlf(int64_t& x, int64_t& offset)
 {
     x >>= 1;
     ++offset;
 }
 
+// Triple value
 __device__ void tpl(int64_t& x, int64_t& offset)
 {
     x *= 3;
     ++offset; 
 }
 
+// Increment value by 1
 __device__ void inc(int64_t& x, int64_t& offset)
 {
     ++x;
     ++offset;
 }
 
+// Jump offset by 19
 __device__ void jmp19(int64_t&, int64_t& offset)
 {
     offset += 19;
 }
 
+// Jump offset by 2
 __device__ void jmp2(int64_t&, int64_t& offset)
 {
     offset += 2;
 }
 
+// Jump offset by -7
 __device__ void jmpneg7(int64_t&, int64_t& offset)
 {
     offset -= 7;
 }
 
+// Conditional jump by 4 if value is even
 __device__ void jie4(int64_t& x, int64_t& offset)
 {
     if (x % 2 == 0)
@@ -50,6 +60,7 @@ __device__ void jie4(int64_t& x, int64_t& offset)
     }
 }
 
+// Conditional jump by 22 if value is 1
 __device__ void jio22(int64_t& x, int64_t& offset)
 {
     if (x == 1)
@@ -62,6 +73,7 @@ __device__ void jio22(int64_t& x, int64_t& offset)
     }
 }
 
+// Conditional jump by 8 if value is 1
 __device__ void jio8(int64_t& x, int64_t& offset)
 {
     if (x == 1)
@@ -74,13 +86,14 @@ __device__ void jio8(int64_t& x, int64_t& offset)
     }
 }
 
+// Input instructions
 __device__ fptr instructions[48] = {
     jio22, inc, tpl, tpl, tpl, inc, tpl, inc, tpl, inc, inc, tpl, inc, inc, tpl,
     inc, inc, tpl, inc, inc, tpl, jmp19, tpl, tpl, tpl, tpl, inc, inc, tpl, inc,
     tpl, inc, inc, tpl, inc, inc, tpl, inc, tpl, tpl, jio8, inc, jie4, tpl, inc,
     jmp2, hlf, jmpneg7};
 
-
+// Execute instructions in order
 __global__ void execute()
 {
     int64_t offset = 0;
@@ -100,6 +113,7 @@ __global__ void execute()
 
 int main()
 {
+    // Part 1
     a = 0;
     b = 0;
     cudaDeviceSynchronize();
@@ -107,6 +121,7 @@ int main()
     cudaDeviceSynchronize();
     std::cout << "Solution 1: " << b << std::endl;
 
+    // Part 2
     a = 1;
     b = 0;
     cudaDeviceSynchronize();
