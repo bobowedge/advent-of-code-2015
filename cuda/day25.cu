2181decf81c4b0fa4d0f260e51336589e93bc63b
bobowedge <mmbowen@gmail.com>
2021-04-07 15:25:52 -0400

    Updates for Day 25

--- cuda/day25.cu
+++ cuda/day25.cu
@@ -1,9 +1,12 @@
 #include <cstdint>
 #include <iostream>
 
+// Input row
 const int64_t row = 2947;
+// Input column
 const int64_t col = 3029;
 
+// Find the entry number of the given row and column in sequence
 int64_t entry(int64_t row, int64_t col)
 {
     int64_t value = col * (col + 1) / 2;
@@ -16,9 +19,9 @@ int64_t entry(int64_t row, int64_t col)
 
 int main()
 {
-    int64_t x = entry(row, col);
+    int64_t entryNum = entry(row, col);
     int64_t value = 20151125;
-    for (int64_t i = 2; i <= x; ++i)
+    for (int64_t i = 2; i <= entryNum; ++i)
     {
         value *= 252533;    
         value %= 33554393;
